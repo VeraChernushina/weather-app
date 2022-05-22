cc71e2f21532a941f11105be9d21788c3e2c17ec
Vera Chernushina <chernushina.vera@gmail.com>
2022-05-22 23:56:25 +0300

    feat: add styles

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
       <div class="search-box">
         <input type="text" name="search-bar" class="search-bar" placeholder="Search..." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Northampton, UK</div>
+          <div class="date">Sunday 22 May 2022</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9 C</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -57,5 +69,43 @@ main {
   outline: none;
   background-color: rgba(255,255,255,0.5);
   border-radius: 0 16px 0 16px;
+}
+
+.location-box {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0,0,0,0.25);
+}
+
+.date {
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+  color: #fff;
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
+}
+
+.temp {
+  display: inline-block;
+  padding: 10px 25px;
+  font-size: 102px;
+  font-weight: 900;
+  background-color: rgba(255,255,255,0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0,0,0,0.25);
+}
+
+.weather {
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
 }
 </style>
