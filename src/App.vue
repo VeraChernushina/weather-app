564e6a2552eed214489d4890c76fb8cdd1d38cb8
Vera Chernushina <chernushina.vera@gmail.com>
2022-05-23 00:23:50 +0300

    feat: make date be dynamic

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">Sunday 22 May 2022</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
 
         <div class="weather-box">
@@ -51,6 +51,19 @@ export default {
 
     setResults(results) {
       this.weather = results;
+    },
+
+    dateBuilder() {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${date} ${month} ${year}`;
     }
   }
 }
